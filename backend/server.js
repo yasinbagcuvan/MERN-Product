d655d4121657ff036aa3d6f679240d6fc53ae288
Yasin Bağçuvan <yasinbagcuvan@gmail.com>
2025-04-23 18:46:53 +0300

    MongoDB bağlantısı kuruldu,  Product için crud işlemler yapıldı.

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+// const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+import productRoutes from "./routes/product.route.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json()); // allows us to accept JSON data in the req.body
+
+app.use("/api/products", productRoutes);
+
+app.listen(5000, () => {
+  connectDB();
+  console.log("Server started at http://localhost:5000 ");
+});

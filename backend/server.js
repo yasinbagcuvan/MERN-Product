b8770db01b9ce12c131055e4d7d5475f530f89dd
Yasin Bağçuvan <yasinbagcuvan@gmail.com>
2025-04-25 00:30:45 +0300

    Mernstack product sale projet complated.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 // const express = require("express");
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 
 import productRoutes from "./routes/product.route.js";
@@ -10,9 +11,19 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
